7ccd020f01ce27dc8f2333140b60fe0e34fdc674
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-07 18:03:50 +0200

    added function onFormSubmit and init

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+{
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    const init = () => {
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", (onFormSubmit));
+    }
+    init();
+
+}
