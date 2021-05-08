c6331b286aee74b75d6557adab7142282360c7b8
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-08 11:03:43 +0200

    added task  , added function addNewTask , render

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 {
+    const tasks = [
+    
+    ];
+
+    const addNewTask = (newTaskContent) =>{
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+    
     const onFormSubmit = (event) => {
         event.preventDefault();
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        };
+        addNewTask(newTaskContent);
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    ${task.content}
+                </li>
+            `;
+        };
+        document.querySelector(".js-task").innerHTML = htmlString;
     }
 
     const init = () => {
+        render();
         const form = document.querySelector(".js-form");
         form.addEventListener("submit", (onFormSubmit));
+
     }
     init();
 
